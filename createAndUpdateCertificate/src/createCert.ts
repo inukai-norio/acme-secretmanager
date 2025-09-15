31346f62c2a4d7a22a893bfa59ed362a618b8268
inukai-norio <jj1uty@jarl.com>
2025-09-15 12:46:45 +0900

    Certificate 発行部分に関して Secrets Managerにすべて入れた

--- createAndUpdateCertificate/src/createCert.ts
+++ createAndUpdateCertificate/src/createCert.ts
@@ -1,7 +1,7 @@
 import { Route53Client, ChangeResourceRecordSetsCommand, ChangeAction } from '@aws-sdk/client-route-53';
 import { crypto, Client, Authorization, CsrBuffer } from 'acme-client';
 import { resolveTxt } from 'node:dns/promises';
-import { Param, createPrivateKey } from './lib.js';
+import { Cert, createPrivateKey } from './lib.js';
 const route53 = new Route53Client();
 
 async function route53Challenge(action: ChangeAction, recordName: string, recordValue: string, hostedZoneId: string) {
@@ -54,20 +54,18 @@ async function waitForDnsPropagation(recordName: string, recordValue: string) {
   throw new Error('timeout');
 }
 
-async function createCrt(client: Client, acmeParam: Param, hostedZoneId: string, csr: CsrBuffer) {
+async function createCrt(client: Client, cert: Cert, csr: CsrBuffer) {
   const mkRecordName = (authz: Authorization) => `_acme-challenge.${authz.identifier.value}.`;
 
   return client.auto({
     csr,
-    email: acmeParam.email,
-    termsOfServiceAgreed: true,
     challengePriority: ['dns-01'],
     challengeCreateFn: async (authz, challenge, keyAuthorization) => {
       if (challenge.type !== 'dns-01') {
         throw new Error(`This code is "dns-01" only supports. (request: "${challenge.type}")`);
       }
       const recordName = mkRecordName(authz);
-      await route53Challenge('UPSERT', recordName, keyAuthorization, hostedZoneId);
+      await route53Challenge('UPSERT', recordName, keyAuthorization, cert.hostedZoneId);
       // dns が更新されるまで待つ
       await waitForDnsPropagation(recordName, keyAuthorization);
     },
@@ -76,24 +74,18 @@ async function createCrt(client: Client, acmeParam: Param, hostedZoneId: string,
         throw new Error(`This code is "dns-01" only supports. (request: "${challenge.type}")`);
       }
       const recordName = mkRecordName(authz);
-      await route53Challenge('DELETE', recordName, keyAuthorization, hostedZoneId);
+      await route53Challenge('DELETE', recordName, keyAuthorization, cert.hostedZoneId);
     },
   });
 }
 
-export async function createCert(client: Client, acmeParam: Param, domain: string, hostedZoneId: string) {
-  const keyPem = await createPrivateKey(acmeParam.priveteKey);
+export async function createCert(client: Client, cert: Cert) {
+  const keyPem = await createPrivateKey(cert.keyConfig);
   const [privateKey, csr] = await crypto.createCsr({
-    commonName: domain,
+    commonName: cert.domain,
   }, keyPem);
 
-  const [key, crt] = await Promise.all([
-    privateKey.toString(),
-    createCrt(client, acmeParam, hostedZoneId, csr),
-  ]);
-
-  return {
-    key,
-    crt,
-  };
+  cert.key = privateKey.toString();
+  cert.crt = await createCrt(client, cert, csr);
+  return cert;
 }
